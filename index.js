26f177294fc762f04f28996e4fad4eaa912d20f5
RifatHossain41 <rifathossain0423@gmail.com>
2023-06-11 09:58:21 +0600

    added suudents collection

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -19,10 +19,32 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const studentsCollection = client.db("SummerDb").collection("students");
+    // const classesCollection = client.db("bistroDb").collection("classes");
+
+    app.get('/students', async(req, res) => {
+      const result = await studentsCollection.find().toArray();
+      res.send(result)
+    });
+
+    app.post('/students', async(req, res) => {
+      const student = req.body;
+      console.log(student);
+      const query  = {email: student.email}
+      const existingStudent = await studentsCollection.findOne(query);
+      if(existingStudent) {
+        return res.send({message: 'Student already exists'})
+      }
+      const result = await studentsCollection.insertOne(student);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
